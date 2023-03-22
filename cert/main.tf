1c986fd72f03dca2440fe80285bd8ca1dcdc44ec
Peter Grasso <petermgrasso@gmail.com>
2023-03-21 21:33:01 -0400

    Removes the data entry for the module

--- cert/main.tf
+++ cert/main.tf
@@ -9,12 +9,6 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-data "aws_route53_zone" "cert" {
-  count        = var.hosted_zone != null ? 1 : 0
-  name         = var.hosted_zone
-  private_zone = var.private
-}
-
 resource "aws_route53_record" "cert" {
   for_each = var.domain == null ? {} : {
     for dvo in aws_acm_certificate.cert[0].domain_validation_options : dvo.domain_name => {
@@ -29,11 +23,11 @@ resource "aws_route53_record" "cert" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.cert[0].zone_id
+  zone_id         = var.hosted_zone
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  count = var.domain != null ? 1 : 0
+  count = var.domain != null && var.hosted_zone != null ? 1 : 0
 
   certificate_arn         = aws_acm_certificate.cert[0].arn
   validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
