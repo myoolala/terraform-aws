b5e5a333f01ed499619fb806d901c28bec89c3cc
Peter Grasso <petermgrasso@gmail.com>
2026-03-03 10:28:32 -0500

    Fixes bug with fargate servie output for the load balancer

--- modules/fargate-service/output.tf
+++ modules/fargate-service/output.tf
@@ -1,5 +1,5 @@
 output "cname_target" {
-  value       = var.lb != null ? module.lb.dns_name : null
+  value       = var.lb != null ? module.lb[0].dns_name : null
   description = "DNS CNAME of the load balancer to reach in order to talk to the service"
 }
 
