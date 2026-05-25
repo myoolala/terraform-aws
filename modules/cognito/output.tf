6bbd9baebd7c7252390a4f147b5af16597631a67
Peter Grasso <petermgrasso@gmail.com>
2026-05-25 19:05:50 -0400

    Adds pool arn to cognito module

--- modules/cognito/output.tf
+++ modules/cognito/output.tf
@@ -2,6 +2,11 @@ output "pool_id" {
   value = aws_cognito_user_pool.this.id
 }
 
+output "pool_arn" {
+  value = aws_cognito_user_pool.this.arn
+  description = "ARN of the created user pool"
+}
+
 output "client_secret" {
   value = var.client_config != null ? aws_cognito_user_pool_client.userpool_client[0].client_secret : 0
 }
