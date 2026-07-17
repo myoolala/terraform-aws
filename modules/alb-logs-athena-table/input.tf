05bf73b2a65b1a777f5575d451d8d0d9c8cbc126
Peter Grasso <petermgrasso@gmail.com>
2026-07-16 21:54:37 -0400

    Ran terraform fmt

--- modules/alb-logs-athena-table/input.tf
+++ modules/alb-logs-athena-table/input.tf
@@ -40,8 +40,8 @@ variable "athena_results_bucket" {
 }
 variable "ser_de_info_regex" {
   description = "Primary regex used to parse the logs. This would probably change with a WAF attached to the ALB"
-  type = string
-  default = "([^ ]+) ([^ ]+) ([^ ]+) ([^: ]+):([0-9]+) ([^ ]+)(?::([0-9]+))? ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) \"([^ ]+) ([^\"]*) ([^ ]+)\" \"([^\"]*)\" ([^ ]+) ([^ ]+) ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([^ ]+) ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\""
+  type        = string
+  default     = "([^ ]+) ([^ ]+) ([^ ]+) ([^: ]+):([0-9]+) ([^ ]+)(?::([0-9]+))? ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) \"([^ ]+) ([^\"]*) ([^ ]+)\" \"([^\"]*)\" ([^ ]+) ([^ ]+) ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([^ ]+) ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([^ ]+) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\""
 }
 
 variable "partition_keys" {
@@ -51,10 +51,10 @@ variable "partition_keys" {
   description = "Parition keys to attach to the table"
   default = {
     account = {},
-    region = {},
-    year = {},
-    month = {},
-    day = {},
+    region  = {},
+    year    = {},
+    month   = {},
+    day     = {},
   }
 }
 
@@ -64,42 +64,42 @@ variable "columns" {
   }))
   description = "Columns in order to match to the regex"
   default = {
-    type = {},
-    time = {},
-    elb = {},
-    client_ip = {},
-    client_port = { type = "int" },
-    target_ip = {},
-    target_port = {},
-    request_processing_time = { type = "double" },
-    target_processing_time = { type = "double" },
+    type                     = {},
+    time                     = {},
+    elb                      = {},
+    client_ip                = {},
+    client_port              = { type = "int" },
+    target_ip                = {},
+    target_port              = {},
+    request_processing_time  = { type = "double" },
+    target_processing_time   = { type = "double" },
     response_processing_time = { type = "double" },
-    elb_status_code = { type = "int" },
-    target_status_code = {},
-    received_bytes = { type = "bigint" },
-    sent_bytes = { type = "bigint" },
-    request_verb = {},
-    request_url = {},
-    request_proto = {},
-    user_agent = {},
-    ssl_cipher = {},
-    ssl_protocol = {},
-    target_group_arn = {},
-    trace_id = {},
-    domain_name = {},
-    chosen_cert_arn = {},
-    matched_rule_priority = {},
-    request_creation_time = {},
-    actions_executed = {},
-    redirect_url = {},
-    lambda_error_reason = {},
-    target_port_list = {},
-    target_status_code_list = {},
-    classification = {},
-    classification_reason = {},
-    conn_trace_id = {},
-    transformed_host = {},
-    transformed_uri = {},
+    elb_status_code          = { type = "int" },
+    target_status_code       = {},
+    received_bytes           = { type = "bigint" },
+    sent_bytes               = { type = "bigint" },
+    request_verb             = {},
+    request_url              = {},
+    request_proto            = {},
+    user_agent               = {},
+    ssl_cipher               = {},
+    ssl_protocol             = {},
+    target_group_arn         = {},
+    trace_id                 = {},
+    domain_name              = {},
+    chosen_cert_arn          = {},
+    matched_rule_priority    = {},
+    request_creation_time    = {},
+    actions_executed         = {},
+    redirect_url             = {},
+    lambda_error_reason      = {},
+    target_port_list         = {},
+    target_status_code_list  = {},
+    classification           = {},
+    classification_reason    = {},
+    conn_trace_id            = {},
+    transformed_host         = {},
+    transformed_uri          = {},
     request_transform_status = {}
   }
 }
@@ -109,5 +109,5 @@ variable "additional_columns" {
     type = optional(string, "string")
   }))
   description = "Additional columns in order to match to the regex at the end of the main columns. Intention is to make adding columns easier"
-  default = {}
+  default     = {}
 }
